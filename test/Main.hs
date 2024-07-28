c977bea40f0023d7c1a224d507a2bec9fbe4d6f2
dopamane <dwc1295@gmail.com>
2024-07-28 01:12:08 -0700

    Add golden files to cabal data fields

--- test/Main.hs
+++ test/Main.hs
@@ -1,9 +1,12 @@
 module Main (main) where
 
+import Paths_yosys_rtl
 import Test.Tasty
 import qualified Test.Yosys.Rtl
 
 main :: IO ()
-main = defaultMain $ testGroup "Test.Yosys"
-  [ testGroup "Rtl" Test.Yosys.Rtl.tests
-  ]
+main = do
+  dataDir <- getDataDir
+  defaultMain $ testGroup "Test.Yosys"
+    [ testGroup "Rtl" $ Test.Yosys.Rtl.tests dataDir
+    ]
